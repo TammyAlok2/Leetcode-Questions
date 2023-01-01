eebcdaa540cfbfbe2fb6c6689a12a0c3eddb0c59
Alok Tamrakar <aloktamrakar2@gmail.com>
2023-01-01 15:16:49 +0530

    intial-commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+let v=prompt("enter your name");
+function lw()
+{
+document.getElementsByTagName("h2").innerHtml="("<h1 className ='neoText'>" + 'WISHING YOU HAPPY JANMASHTAMI' + '<br>' +   v   + '</h1>')"
+}
